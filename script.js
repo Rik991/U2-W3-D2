48ac35e1fa5ce6d1d08e48ac93c47a3850ee23da
Riccardo Santilli <riccardosantilli@live.it>
2024-09-24 15:00:00 +0200

    second commit

--- script.js
+++ script.js
@@ -13,17 +13,40 @@ myForm.addEventListener("submit", (e) => {
 
 const saveName = () => {
   nameInput = document.getElementById("userName").value;
-  localStorage.setItem(localStorageInput, nameInput);
-  oldName.innerText = localStorage.getItem(localStorageInput);
-  myForm.appendChild(oldName);
+  if (nameInput) {
+    localStorage.setItem(localStorageInput, nameInput);
+    oldName.innerText = localStorage.getItem(localStorageInput);
+    myForm.appendChild(oldName);
+  } else {
+    alert("scrivi qualcosa zio");
+  }
 };
 
 const clearName = () => {
-  localStorage.removeItem(localStorageInput);
-  oldName.innerText = "";
+  if (localStorage.getItem(localStorageInput)) {
+    localStorage.removeItem(localStorageInput);
+    oldName.innerText = "";
+  } else {
+    alert("nessun dato salvato in localStorage");
+  }
 };
 
 buttonSave.addEventListener("click", saveName);
 buttonClear.addEventListener("click", clearName);
 
 // esercizio 2
+
+const contatore = () => {
+  let tempoTrascorso = parseInt(sessionStorage.getItem("timeElapsed")) || 0; // Ottiene il tempo attuale o lo imposta a 0 se non esiste
+
+  // Funzione per aggiornare il contatore
+  function updateCounter() {
+    tempoTrascorso++; // Incrementa il tempo trascorso di 1 secondo
+    sessionStorage.setItem("timeElapsed", tempoTrascorso); // Salva il valore in sessionStorage
+    document.getElementById("timer").innerText = tempoTrascorso; // Aggiorna il DOM
+  }
+
+  // Aggiorna il contatore ogni secondo
+  setInterval(updateCounter, 1000);
+};
+contatore();
